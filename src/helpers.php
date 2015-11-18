cd0769663c77d8d99109f9b3e19c2c1b18738c3f
Robert Kummer <github@mail.robert-kummer.de>
2015-11-18 20:23:38 +0100

    added missing env file

---
+++ src/helpers.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Adding support for the missing env() method on laravel 4.
+ *
+ * taken from Illuminate/Foundation/helpers.php from laravel 5.
+ */
+
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return value($default);
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        if (Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
+}
